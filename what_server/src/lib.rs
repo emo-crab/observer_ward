8178bdf9438b5fe09071f3d9ef2846b0d37b57e1
cn-kali-team <kali-team@qq.com>
2021-12-16 17:07:09 +0800

    what_server

--- what_server/src/lib.rs
+++ what_server/src/lib.rs
@@ -1,7 +1,215 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io::{BufRead, Write};
+use std::net::TcpStream;
+use std::path::{Path, PathBuf};
+use std::{io, net::SocketAddr, time::Duration};
+
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
+use regex::bytes::Regex;
+use serde::{Deserialize, Serialize};
+
+use unescape_lib::unescape_func;
+
+mod unescape_lib;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Matches {
+    service: String,
+    #[serde(deserialize_with = "unescape_func")]
+    pattern: Vec<u8>,
+    version_info: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct NmapFingerPrintLib {
+    matches: Vec<Matches>,
+    directive_name: String,
+    protocol: String,
+    #[serde(deserialize_with = "unescape_func")]
+    directive_str: Vec<u8>,
+    total_wait_ms: Option<u8>,
+    tcp_wrapped_ms: Option<u8>,
+    #[serde(default)]
+    rarity: u8,
+    #[serde(default)]
+    ports: Vec<u16>,
+    ssl_ports: Option<String>,
+    fallback: Option<String>,
+}
+
+impl NmapFingerPrintLib {
+    pub async fn match_rules(&self, response: &Vec<u8>) -> Vec<String> {
+        let mut server_name: Vec<String> = Vec::new();
+        let mut futures = FuturesUnordered::new();
+        let mut matches_iter = self.matches.iter();
+        for _ in 0..100 {
+            if let Some(rule) = matches_iter.next() {
+                futures.push(self.what_server(&rule, response));
+            } else {
+                break;
+            }
+        }
+        while let Some(result) = futures.next().await {
+            if let Some(rule) = matches_iter.next() {
+                futures.push(self.what_server(&rule, response));
+            }
+            if !result.is_empty() {
+                server_name.push(result);
+            }
+        }
+        if !server_name.is_empty() {
+            println!("{:?}", server_name);
+        }
+        server_name
+    }
+    async fn what_server(&self, rule: &Matches, text: &Vec<u8>) -> String {
+        let regex_str = std::str::from_utf8(&rule.pattern);
+        if let Ok(ok_regex_str) = regex_str {
+            return match Regex::new(&ok_regex_str) {
+                Ok(re) => {
+                    if re.captures(text).is_some() {
+                        rule.service.clone()
+                    } else {
+                        String::new()
+                    }
+                }
+                Err(_) => String::new(),
+            };
+        }
+        String::new()
     }
 }
+
+lazy_static! {
+    static ref NMAP_FINGERPRINT_LIB_DATA: Vec<NmapFingerPrintLib> = {
+        let self_path: PathBuf = env::current_exe().unwrap_or(PathBuf::new());
+        let path = Path::new(&self_path).parent().unwrap_or(Path::new(""));
+        let mut file = match File::open(path.join("nmap-service-probes.json")) {
+            Err(_) => {
+                println!("The nmap fingerprint library cannot be found in the current directory!");
+                std::process::exit(0);
+            }
+            Ok(file) => file,
+        };
+        let mut data = String::new();
+        file.read_to_string(&mut data).unwrap();
+        let nmap_fingerprint: Vec<NmapFingerPrintLib> =
+            serde_json::from_str(&data).expect("BAD JSON");
+        nmap_fingerprint
+    };
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct WhatServer {
+    timeout: u64,
+}
+
+impl WhatServer {
+    pub fn new(timeout: u64) -> Self {
+        Self { timeout }
+    }
+    fn filter_probes_by_port(
+        &self,
+        port: u16,
+    ) -> (Vec<NmapFingerPrintLib>, Vec<NmapFingerPrintLib>) {
+        let (mut in_probes, mut ex_probes): (Vec<NmapFingerPrintLib>, Vec<NmapFingerPrintLib>) =
+            (vec![], vec![]);
+        for nmap_fingerprint in NMAP_FINGERPRINT_LIB_DATA.clone().into_iter() {
+            if nmap_fingerprint.ports.contains(&port) {
+                in_probes.push(nmap_fingerprint);
+            } else {
+                ex_probes.push(nmap_fingerprint);
+            }
+        }
+        return (in_probes, ex_probes);
+    }
+    fn send_directive_str_request(&self, socket: SocketAddr, payload: Vec<u8>) -> Vec<u8> {
+        let received: Vec<u8> = Vec::new();
+        if let Ok(mut stream) = self.connect(socket) {
+            stream
+                .set_write_timeout(Some(Duration::from_millis(self.timeout)))
+                .unwrap_or_default();
+            stream
+                .set_read_timeout(Some(Duration::from_millis(self.timeout)))
+                .unwrap_or_default();
+            stream.write_all(&payload).unwrap();
+            stream.flush().unwrap();
+            let mut reader = io::BufReader::new(&mut stream);
+            let received: Vec<u8> = reader.fill_buf().unwrap_or_default().to_vec();
+            reader.consume(received.len());
+            return received;
+        };
+        received
+    }
+
+    fn connect(&self, socket: SocketAddr) -> io::Result<TcpStream> {
+        let stream = TcpStream::connect(socket).unwrap();
+        stream.set_nodelay(true).unwrap();
+        stream.set_ttl(100).unwrap();
+        Ok(stream)
+    }
+    async fn exec_run(&self, probe: NmapFingerPrintLib, host_port: SocketAddr) -> Vec<String> {
+        let response = self.send_directive_str_request(host_port, probe.directive_str.clone());
+        let server = probe.match_rules(&response).await;
+        return server;
+    }
+    pub async fn scan(&self, host_port: SocketAddr) -> HashSet<String> {
+        let (in_probes, ex_probes) = self.filter_probes_by_port(host_port.port());
+        let mut in_probes_iter = in_probes.into_iter();
+        let mut ex_probes_iter = ex_probes.into_iter();
+        let mut futures = FuturesUnordered::new();
+        let mut server_set: HashSet<String> = HashSet::new();
+        for _ in 0..16 {
+            if let Some(socket) = in_probes_iter.next() {
+                futures.push(self.exec_run(socket, host_port));
+            } else {
+                break;
+            }
+        }
+        while let Some(result) = futures.next().await {
+            if let Some(probes) = in_probes_iter.next() {
+                futures.push(self.exec_run(probes, host_port));
+            }
+            if !result.is_empty() {
+                server_set.extend(result);
+            }
+        }
+        if !server_set.is_empty() {
+            return server_set;
+        }
+        let mut futures = FuturesUnordered::new();
+        for _ in 0..16 {
+            if let Some(probes) = ex_probes_iter.next() {
+                futures.push(self.exec_run(probes, host_port));
+            }
+        }
+        while let Some(result) = futures.next().await {
+            if let Some(socket) = ex_probes_iter.next() {
+                futures.push(self.exec_run(socket, host_port));
+            }
+            if !result.is_empty() {
+                server_set.extend(result);
+            }
+        }
+        return server_set;
+    }
+}
+
+// use std::net::{IpAddr, SocketAddr};
+// use what_server::WhatServer;
+//
+// #[tokio::main]
+// async fn main() {
+//     let ip = "127.0.0.1".parse::<IpAddr>().unwrap();
+//     let socket = SocketAddr::new(ip, 22);
+//     let what_server = WhatServer::new(300);
+//     let server = what_server.scan(socket).await;
+//     println!("{:?}", server);
+// }
---
+++ what_server/src/unescape_lib.rs
@@ -0,0 +1,227 @@
+//! Utilities for validating string and char literals and turning them into
+//! values they represent.
+
+use std::fmt;
+use std::marker::PhantomData;
+use std::str::Chars;
+
+use serde::{de, Deserialize, Deserializer};
+
+#[derive(Debug, PartialEq, Eq)]
+enum EscapeError {
+    LoneSlash,
+    InvalidEscape,
+    BareCarriageReturn,
+    EscapeOnlyChar,
+
+    TooShortHexEscape,
+    InvalidCharInHexEscape,
+    OutOfRangeHexEscape,
+
+    NoBraceInUnicodeEscape,
+    InvalidCharInUnicodeEscape,
+    EmptyUnicodeEscape,
+    UnclosedUnicodeEscape,
+    LeadingUnderscoreUnicodeEscape,
+    OverlongUnicodeEscape,
+    LoneSurrogateUnicodeEscape,
+    OutOfRangeUnicodeEscape,
+
+    UnicodeEscapeInByte,
+    NonAsciiCharInByte,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Mode {
+    ByteStr,
+}
+
+impl Mode {
+    pub fn in_single_quotes(self) -> bool {
+        match self {
+            Mode::ByteStr => false,
+        }
+    }
+
+    pub fn in_double_quotes(self) -> bool {
+        !self.in_single_quotes()
+    }
+
+    pub fn is_bytes(self) -> bool {
+        match self {
+            Mode::ByteStr => true,
+        }
+    }
+}
+
+fn scan_escape(first_char: char, chars: &mut Chars<'_>, mode: Mode) -> Result<char, EscapeError> {
+    if first_char != '\\' {
+        return match first_char {
+            '\t' | '\n' => Err(EscapeError::EscapeOnlyChar),
+            '\r' => Err(EscapeError::BareCarriageReturn),
+            '\'' if mode.in_single_quotes() => Err(EscapeError::EscapeOnlyChar),
+            '"' if mode.in_double_quotes() => Err(EscapeError::EscapeOnlyChar),
+            _ => {
+                if mode.is_bytes() && !first_char.is_ascii() {
+                    return Err(EscapeError::NonAsciiCharInByte);
+                }
+                Ok(first_char)
+            }
+        };
+    }
+    let second_char = chars.next().ok_or(EscapeError::LoneSlash)?;
+    let res = match second_char {
+        '"' => '"',
+        'n' => '\n',
+        'r' => '\r',
+        't' => '\t',
+        '\\' => '\\',
+        '\'' => '\'',
+        '0' => '\0',
+
+        'x' => {
+            let hi = chars.next().ok_or(EscapeError::TooShortHexEscape)?;
+            let hi = hi.to_digit(16).ok_or(EscapeError::InvalidCharInHexEscape)?;
+
+            let lo = chars.next().ok_or(EscapeError::TooShortHexEscape)?;
+            let lo = lo.to_digit(16).ok_or(EscapeError::InvalidCharInHexEscape)?;
+
+            let value = hi * 16 + lo;
+
+            if !mode.is_bytes() && !is_ascii(value) {
+                return Err(EscapeError::OutOfRangeHexEscape);
+            }
+            let value = value as u8;
+
+            value as char
+        }
+
+        'u' => {
+            if chars.next() != Some('{') {
+                return Err(EscapeError::NoBraceInUnicodeEscape);
+            }
+
+            let mut n_digits = 1;
+            let mut value: u32 = match chars.next().ok_or(EscapeError::UnclosedUnicodeEscape)? {
+                '_' => return Err(EscapeError::LeadingUnderscoreUnicodeEscape),
+                '}' => return Err(EscapeError::EmptyUnicodeEscape),
+                c => c
+                    .to_digit(16)
+                    .ok_or(EscapeError::InvalidCharInUnicodeEscape)?,
+            };
+
+            loop {
+                match chars.next() {
+                    None => return Err(EscapeError::UnclosedUnicodeEscape),
+                    Some('_') => continue,
+                    Some('}') => {
+                        if n_digits > 6 {
+                            return Err(EscapeError::OverlongUnicodeEscape);
+                        }
+                        if mode.is_bytes() {
+                            return Err(EscapeError::UnicodeEscapeInByte);
+                        }
+
+                        break std::char::from_u32(value).ok_or_else(|| {
+                            if value > 0x10FFFF {
+                                EscapeError::OutOfRangeUnicodeEscape
+                            } else {
+                                EscapeError::LoneSurrogateUnicodeEscape
+                            }
+                        })?;
+                    }
+                    Some(c) => {
+                        let digit = c
+                            .to_digit(16)
+                            .ok_or(EscapeError::InvalidCharInUnicodeEscape)?;
+                        n_digits += 1;
+                        if n_digits > 6 {
+                            continue;
+                        }
+                        let digit = digit as u32;
+                        value = value * 16 + digit;
+                    }
+                };
+            }
+        }
+        _ => {
+            if first_char == '\\' {
+                second_char
+            } else {
+                return Err(EscapeError::InvalidEscape);
+            }
+        }
+    };
+    Ok(res)
+}
+
+/// Takes a contents of a string literal (without quotes) and produces a
+/// sequence of escaped characters or errors.
+fn unescape_str_or_byte_str(src: &str) -> Vec<u8> {
+    let mode = Mode::ByteStr;
+    let mut chars = src.chars();
+    let mut buf = Vec::with_capacity(src.len());
+    while let Some(first_char) = chars.next() {
+        let unescaped_char = match first_char {
+            '\\' => {
+                let second_char = chars.clone().next();
+                match second_char {
+                    Some('\n') => {
+                        skip_ascii_whitespace(&mut chars);
+                        continue;
+                    }
+                    _ => scan_escape(first_char, &mut chars, mode),
+                }
+            }
+            '\n' => Ok('\n'),
+            '\t' => Ok('\t'),
+            _ => scan_escape(first_char, &mut chars, mode),
+        };
+        let unescaped_char_result = unescaped_char.unwrap_or_default();
+        if !unescaped_char_result.is_control() && first_char == '\\' {
+            buf.push('\\' as u8);
+        }
+        buf.push(unescaped_char_result as u8);
+        // callback(start..end, unescaped_char);
+    }
+    return buf;
+
+    fn skip_ascii_whitespace(chars: &mut Chars<'_>) {
+        let str = chars.as_str();
+        let first_non_space = str
+            .bytes()
+            .position(|b| b != b' ' && b != b'\t' && b != b'\n' && b != b'\r')
+            .unwrap_or(str.len());
+        *chars = str[first_non_space..].chars()
+    }
+}
+
+fn is_ascii(x: u32) -> bool {
+    x <= 0x7F
+}
+
+pub fn unescape_func<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct StringToHashSet(PhantomData<Vec<u8>>);
+    impl<'de> de::Visitor<'de> for StringToHashSet {
+        type Value = Vec<u8>;
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("string or list of strings")
+        }
+        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            Ok(unescape_str_or_byte_str(value))
+        }
+        fn visit_seq<S>(self, visitor: S) -> Result<Self::Value, S::Error>
+        where
+            S: de::SeqAccess<'de>,
+        {
+            Deserialize::deserialize(de::value::SeqAccessDeserializer::new(visitor))
+        }
+    }
+    deserializer.deserialize_any(StringToHashSet(PhantomData))
+}
