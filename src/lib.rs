770ae1f5d413df074136f195cc7afcf67d5966a4
cn-kali-team <kali-team@qq.com>
2022-04-01 15:14:10 +0800

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -349,8 +349,8 @@ pub fn read_file_to_target(file_path: &str) -> HashSet<String> {
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
+where
+    P: AsRef<Path>,
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
